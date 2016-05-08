469dcbaad2a2154c1bbc844fa6224d80eb0b36a4
andrewLA331 <andrew.allen2@yahoo.com>
2016-05-08 18:05:21 -0400

    Update home.php

--- home.php
+++ home.php
@@ -2,6 +2,7 @@
 
 <h2>About the Site</h2>
 
+
 <p>This project is a redesign for the the UVM cycling club team website. 
 The end goal is to provide a web portal for prospective club members to find out more about the team and contact a representative, 
 find out about rides/races, read race reports, and to provide team members with a way to post/look at rides, 
@@ -9,3 +10,6 @@ post/view race reports/blog posts, and have an online presence.</p>
 
 <h2>Documentation</h2>
 <embed src="CS142AdvancedWebDesignCSSandUsabilitySpring2016%20(1)%20(1).pdf"final.pdf"" width="500px" height="800px">
+
+<h2>Usability Tests</h2>
+
