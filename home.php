2f90073e08c1172daf9913c134d0b00a009467e3
andrewLA331 <andrewallen@Andrews-MacBook-Pro-2.local>
2016-05-08 17:30:54 -0400

    home.php
    Code

---
+++ home.php
@@ -0,0 +1,10 @@
+<h1>Final Project Homepage</h1>
+
+<h2>About the Site</h2>
+<p> </p>
+
+<h2>Documentation</h2>
+<embed src="final.pdf" width="500px" height="800px">
+
+<h2>Storyboard</h2>
+<embed src="CS%20142%20Final%20Project.pdf.ai" width="500px" height="800px">
