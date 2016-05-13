ec9ecbc1120efd7bd69e6e7b03824c12e1c09315
andrewLA331 <andrewallen@Andrews-MacBook-Pro-2.local>
2016-05-13 19:55:37 -0400

    home

--- home.php
+++ home.php
@@ -1,3 +1,8 @@
+<head>
+    <link rel="stylesheet" href="home.css">
+    <link href='https://fonts.googleapis.com/css?family=Permanent+Marker' rel='stylesheet' type='text/css'>
+</head>
+<body>
 <h1>Final Project Homepage</h1>
 
 <h2>About the Site</h2>
@@ -9,7 +14,28 @@ find out about rides/races, read race reports, and to provide team members with 
 post/view race reports/blog posts, and have an online presence.</p>
 
 <h2>Documentation</h2>
+<<<<<<< Updated upstream
 <embed src="CS142AdvancedWebDesignCSSandUsabilitySpring2016%20(1)%20(1).pdf"final.pdf"" width="500px" height="800px">
 
 <h2>Usability Tests</h2>
 
+=======
+<embed src="CS142AdvancedWebDesignCSSandUsabilitySpring2016%20(1)%20(1).pdf"final.pdf"" width="800px" height="800px">
+    
+<h2>Description</h2>
+<p>We wanted to make this site very modern and sleek while sticking to the UVM cycling team style. The fonts and colors are the ones used by the team. The big sweeping photos and general layout of the site is in tune with other professional cycling team sites. There were no major problems or issues with the website mentioned by the usability testers or classmates when we presented, because we designed and developed the site with the user in mind.The only thing that was mentioned was the navigation font was hard to read so we spaced out the letters. The sight is very simple to use and the navigatation links are named to perfectly describe what the page is for. Each page has the title so that the user always knows where they are.</p>
+
+<h2>Usability Tests</h2>
+    <div>
+    <video width="560" height="315" controls>
+  <source src="UVMusability.webm" type="video/webm"> 
+    </video>
+        <p>Date- 5/8/16 </p>
+        <p>Summary: The only problem the tester had with the site was the font used in the navigation. Said it was hard to read. Other than that, there were no other issues with the site.</p>
+    </div>
+    
+    <div>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/XW0MPZvA_gk" frameborder="0" allowfullscreen></iframe>
+    </div>
+</body>
+>>>>>>> Stashed changes
